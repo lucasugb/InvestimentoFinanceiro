37c5c7d18a3cc89f6763d1067b6474c4edfc5a53
lucasugb <2024101541@academicougb.com.br>
2025-09-10 17:46:53 -0300

    Adicionar arquivos de projeto.

---
+++ Investimento Financeiro/Program.cs
@@ -0,0 +1,11 @@
+﻿using Investimento_Financeiro.TesouroSelic;
+
+Console.WriteLine("Informe o capital inicial: ");
+double capitalInicial = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Informe a taxa anual: ");
+double taxaAnual = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Informe o período em anos: ");
+int periodo = Convert.ToInt32(Console.ReadLine());
+
+decimal montante = TesouroSelic.Calcular(capitalInicial, taxaAnual, periodo);
+Console.WriteLine(montante);
---
+++ Investimento Financeiro/investimentos/TesouroSelic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investimento_Financeiro.TesouroSelic
+{
+    public class TesouroSelic
+    {
+        public static decimal Calcular(double capitalInicial, double taxaAnual, int periodo)
+        {
+            double montante = capitalInicial * Math.Pow(1 + taxaAnual, periodo);
+            return (decimal)montante;
+        }
+    }
+}
